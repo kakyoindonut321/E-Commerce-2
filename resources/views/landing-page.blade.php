af80ee81fef76efeec257127b71db308c387b3b8
rifkirmdn1 <rmrifkir84@gmail.com>
2022-10-27 19:11:19 +0700

    nambah landing page

---
+++ resources/views/landing-page.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="landing.css">
+    <title>E-commerce</title>
+</head>
+<body>
+    <nav>
+        <div class="top-menu">
+            
+            <img src="Logo (2) 2.png" alt="Logo">
+
+            <ul class="navbar">
+                <div class="start-now">
+                    <li><a href="#"><b>START NOW</b></a></li>
+                </div>
+                <div class="login">
+                    <li><a href="#"><b>LOGIN</b></a></li>
+                </div>
+            </ul>
+        </div>
+    </nav>
+    <div class="hero">
+        <img src="modem 1.png" alt="modem" class="img-hero">
+    </div>
+
+     
+
+
+</body>
+</html>
