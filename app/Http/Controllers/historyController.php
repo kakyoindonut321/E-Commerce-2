052e64dd4882fcacda542578af94ce092909831c
kakyoindonut321 <forbiddenikki@gmail.com>
2022-11-03 17:41:22 +0700

    fix delete + add update in product controller

---
+++ app/Http/Controllers/historyController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\History;
+use Illuminate\Http\Request;
+
+class historyController extends Controller
+{
+    public function index() {
+        return view('user.order', [
+            'title' => 'History',
+            'history' => History::with('product', 'user')->where('user_id', auth()->user()->id)->get()  
+        ]);
+    }
+
+}
